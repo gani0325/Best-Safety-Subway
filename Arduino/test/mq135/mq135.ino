111e8c0975ded2cb4cbc56fc7553918d44445a5a
gani0325 <lge0325@bme.gachon.ac.kr>
2023-12-14 10:50:04 +0900

    ➕[Feat] gasSensor.getPPM(); 로 co2 측정하기

--- Arduino/test/mq135/mq135.ino
+++ Arduino/test/mq135/mq135.ino
@@ -14,16 +14,6 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-// Declaration for SSD1306 display connected using software SPI (default case):
-#define OLED_MOSI   9
-#define OLED_CLK   10
-#define OLED_DC    11
-#define OLED_CS    12
-#define OLED_RESET 13
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT,OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
-
 MQ135 gasSensor = MQ135(A0);
 int val;
 int sensorPin = A0;
@@ -33,10 +23,6 @@ void setup() {
   Serial.begin(9600);
 
   pinMode(sensorPin, INPUT);
-
-  display.begin(SSD1306_SWITCHCAPVCC);
-  display.clearDisplay();
-  display.display();
 }
 
 void loop() {
@@ -50,18 +36,6 @@ void loop() {
   float ppm = gasSensor.getPPM();
   Serial.print ("ppm: ");
   Serial.println (ppm);
-
-  display.setTextSize(2);
-  display.setTextColor(WHITE);
-  display.setCursor(18,43);
-  display.println("CO2");
-  display.setCursor(63,43);
-  display.println("(PPM)");
-  display.setTextSize(2);
-  display.setCursor(28,5);
-  display.println(ppm);
-  display.display();
-  display.clearDisplay();
 
   delay(2000);
 }
